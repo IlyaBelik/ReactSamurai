dcac794c40a0387486ba2a4831adb1f5a9230c94
IlyaBelik <beleykanich@inbox.ru>
2020-12-06 18:46:30 +0200

    Lesson 1-13

---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Profile = () => {
+    return <div className='profile'>
+        <div>
+            <img src="https://img4.goodfon.ru/original/1920x1080/2/81/firewatch-campo-santo-kholmy-vid-les-igra-peizazh-gory-poz-8.jpg" alt=""/>
+        </div>
+        <div>
+            avatar and descr
+        </div>
+        <div>
+            My Posts
+            <div>
+                New Post
+            </div>
+            <div>
+                Post 1
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile
