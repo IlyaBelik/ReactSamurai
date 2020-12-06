f180d455372596a11cc26030428e3517232450b6
IlyaBelik <beleykanich@inbox.ru>
2020-12-07 00:12:31 +0200

    test

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -4,7 +4,11 @@ import s from './Post.module.css'
 const Post = () => {
     return <div className={s.item}>
                 Post 1
+        <div>
+            ало
+        </div>
             </div>
+
 
 }
 
