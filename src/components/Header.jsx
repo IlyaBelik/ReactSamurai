51bd2fe196c6514573909e6f0840038a9ddb643b
IlyaBelik <beleykanich@inbox.ru>
2020-12-06 22:31:43 +0200

    Module css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from'../styles/Header.module.css'
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img src="https://png.pngtree.com/element_pic/16/11/02/bd886d7ccc6f8dd8db17e841233c9656.jpg" alt=""/>
         </header>
     )
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
+import s from '../styles/Profile.module.css'
 
 const Profile = () => {
-    return <div className='profile'>
+    return <div className={s.profile}>
         <div>
             <img src="https://img4.goodfon.ru/original/1920x1080/2/81/firewatch-campo-santo-kholmy-vid-les-igra-peizazh-gory-poz-8.jpg" alt=""/>
         </div>
         <div>
             avatar and descr
         </div>
-        <div>
+        <div className={s.posts}>
             My Posts
             <div>
                 New Post
             </div>
-            <div>
+            <div className={s.item}>
                 Post 1
             </div>
         </div>
